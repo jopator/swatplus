339520c420b75b81584825f72bfbd8aa6b3965fe
jopator <jose.pablo.teran.orsini@vub.be>
2026-08-28 18:17:10 +0200

    Deleted gen_mod file that was done for single compile test

--- cli_ncdf_meas__genmod.f90
+++
@@ -1,9 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Thu Aug 27 13:22:25 2026
-        ! This source file is for reference only and may not completely
-        ! represent the generated interface used by the compiler.
-        MODULE CLI_NCDF_MEAS__genmod
-          INTERFACE 
-            SUBROUTINE CLI_NCDF_MEAS
-            END SUBROUTINE CLI_NCDF_MEAS
-          END INTERFACE 
-        END MODULE CLI_NCDF_MEAS__genmod
